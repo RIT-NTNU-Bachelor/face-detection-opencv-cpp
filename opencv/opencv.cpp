f353e89251d88a01bb5e15183f1004a898b67a6c
martinhejo <marhjoh@stud.ntnu.no>
2024-03-28 21:14:30 -0400

    add: run-all comparison

--- opencv/opencv.cpp
+++ opencv/opencv.cpp
@@ -1,120 +1,333 @@
 #include <iostream>
-#include <opencv2/opencv.hpp>
-#include <cmath> // For std::round
+#include <string>
+#include <vector>
+#include <stdlib.h>
 
-using namespace std;
+#include <opencv2/core.hpp>
+#include <opencv2/core/version.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/objdetect.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/dnn.hpp>
+
+using namespace cv::dnn;
+
+#include <C:/dlib-19.24/dlib/opencv.h>
+#include <C:/dlib-19.24/dlib/image_processing.h>
+#include <C:/dlib-19.24/dlib/dnn.h>
+#include <C:/dlib-19.24/dlib/data_io.h>
+#include <C:/dlib-19.24/dlib/image_processing/frontal_face_detector.h>
+
+using namespace dlib;
+
+#include <boost/algorithm/string.hpp>
+
 using namespace cv;
+using namespace std;
 
-// Function to calculate the distance from the camera to the face
-float calculateDistance(int faceWidth, int knownWidth, float focalLength) {
-    return (knownWidth * focalLength) / faceWidth;
+/** Global variables */
+String faceCascadePath;
+CascadeClassifier faceCascade;
+
+void detectFaceOpenCVHaar(CascadeClassifier faceCascade, Mat& frameOpenCVHaar, int inHeight = 300, int inWidth = 0)
+{
+    int frameHeight = frameOpenCVHaar.rows;
+    int frameWidth = frameOpenCVHaar.cols;
+    if (!inWidth)
+        inWidth = (int)((frameWidth / (float)frameHeight) * inHeight);
+
+    float scaleHeight = frameHeight / (float)inHeight;
+    float scaleWidth = frameWidth / (float)inWidth;
+
+    Mat frameOpenCVHaarSmall, frameGray;
+    resize(frameOpenCVHaar, frameOpenCVHaarSmall, Size(inWidth, inHeight));
+    cvtColor(frameOpenCVHaarSmall, frameGray, COLOR_BGR2GRAY);
+
+    std::vector<Rect> faces;
+    faceCascade.detectMultiScale(frameGray, faces);
+
+    for (size_t i = 0; i < faces.size(); i++)
+    {
+        int x1 = (int)(faces[i].x * scaleWidth);
+        int y1 = (int)(faces[i].y * scaleHeight);
+        int x2 = (int)((faces[i].x + faces[i].width) * scaleWidth);
+        int y2 = (int)((faces[i].y + faces[i].height) * scaleHeight);
+        cv::rectangle(frameOpenCVHaar, Point(x1, y1), Point(x2, y2), Scalar(0, 255, 0), (int)(frameHeight / 150.0), 4);
+    }
 }
 
-int main() {
-    // Change the path to the location of your video file
-    string videoFilePath = "../videos/classroom_30FPS.mp4";
-    VideoCapture cap(videoFilePath);
+const size_t inWidth = 300;
+const size_t inHeight = 300;
+const double inScaleFactor = 1.0;
+const float confidenceThreshold = 0.7;
+const cv::Scalar meanVal(104.0, 177.0, 123.0);
 
-    if (!cap.isOpened()) {
-        cerr << "ERROR: Could not open video file." << endl;
+const std::string caffeConfigFile = "../models/deploy.prototxt";
+const std::string caffeWeightFile = "../models/res10_300x300_ssd_iter_140000_fp16.caffemodel";
+
+const std::string tensorflowConfigFile = "../models/opencv_face_detector.pbtxt";
+const std::string tensorflowWeightFile = "../models/opencv_face_detector_uint8.pb";
+
+void detectFaceOpenCVDNN(Net net, Mat& frameOpenCVDNN, string framework = "caffe")
+{
+    int frameHeight = frameOpenCVDNN.rows;
+    int frameWidth = frameOpenCVDNN.cols;
+    cv::Mat inputBlob;
+    if (framework == "caffe")
+        inputBlob = cv::dnn::blobFromImage(frameOpenCVDNN, inScaleFactor, cv::Size(inWidth, inHeight), meanVal, false, false);
+    else
+        inputBlob = cv::dnn::blobFromImage(frameOpenCVDNN, inScaleFactor, cv::Size(inWidth, inHeight), meanVal, true, false);
+
+    net.setInput(inputBlob, "data");
+    cv::Mat detection = net.forward("detection_out");
+
+    cv::Mat detectionMat(detection.size[2], detection.size[3], CV_32F, detection.ptr<float>());
+
+    for (int i = 0; i < detectionMat.rows; i++)
+    {
+        float confidence = detectionMat.at<float>(i, 2);
+
+        if (confidence > confidenceThreshold)
+        {
+            int x1 = static_cast<int>(detectionMat.at<float>(i, 3) * frameWidth);
+            int y1 = static_cast<int>(detectionMat.at<float>(i, 4) * frameHeight);
+            int x2 = static_cast<int>(detectionMat.at<float>(i, 5) * frameWidth);
+            int y2 = static_cast<int>(detectionMat.at<float>(i, 6) * frameHeight);
+
+            cv::rectangle(frameOpenCVDNN, cv::Point(x1, y1), cv::Point(x2, y2), cv::Scalar(0, 255, 0), (int)(frameHeight / 150.0), 4);
+        }
+    }
+}
+
+void detectFaceDlibHog(frontal_face_detector hogFaceDetector, Mat& frameDlibHog, int inHeight = 300, int inWidth = 0)
+{
+
+    int frameHeight = frameDlibHog.rows;
+    int frameWidth = frameDlibHog.cols;
+    if (!inWidth)
+        inWidth = (int)((frameWidth / (float)frameHeight) * inHeight);
+
+    float scaleHeight = frameHeight / (float)inHeight;
+    float scaleWidth = frameWidth / (float)inWidth;
+
+    Mat frameDlibHogSmall;
+    resize(frameDlibHog, frameDlibHogSmall, Size(inWidth, inHeight));
+
+    // Convert OpenCV image format to Dlib's image format
+    cv_image<bgr_pixel> dlibIm(frameDlibHogSmall);
+
+    // Detect faces in the image
+    std::vector<dlib::rectangle> faceRects = hogFaceDetector(dlibIm);
+
+    for (size_t i = 0; i < faceRects.size(); i++)
+    {
+        int x1 = (int)(faceRects[i].left() * scaleWidth);
+        int y1 = (int)(faceRects[i].top() * scaleHeight);
+        int x2 = (int)(faceRects[i].right() * scaleWidth);
+        int y2 = (int)(faceRects[i].bottom() * scaleHeight);
+        cv::rectangle(frameDlibHog, Point(x1, y1), Point(x2, y2), Scalar(0, 255, 0), (int)(frameHeight / 150.0), 4);
+    }
+}
+
+// Network Definition
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+template <long num_filters, typename SUBNET> using con5d = con<num_filters, 5, 5, 2, 2, SUBNET>;
+template <long num_filters, typename SUBNET> using con5 = con<num_filters, 5, 5, 1, 1, SUBNET>;
+
+template <typename SUBNET> using downsampler = relu<affine<con5d<32, relu<affine<con5d<32, relu<affine<con5d<16, SUBNET>>>>>>>>>;
+template <typename SUBNET> using rcon5 = relu<affine<con5<45, SUBNET>>>;
+
+using net_type = loss_mmod<con<1, 9, 9, 1, 1, rcon5<rcon5<rcon5<downsampler<input_rgb_image_pyramid<pyramid_down<6>>>>>>>>;
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+void detectFaceDlibMMOD(net_type mmodFaceDetector, Mat& frameDlibMmod, int inHeight = 300, int inWidth = 0)
+{
+
+    int frameHeight = frameDlibMmod.rows;
+    int frameWidth = frameDlibMmod.cols;
+    if (!inWidth)
+        inWidth = (int)((frameWidth / (float)frameHeight) * inHeight);
+
+    float scaleHeight = frameHeight / (float)inHeight;
+    float scaleWidth = frameWidth / (float)inWidth;
+
+    Mat frameDlibMmodSmall;
+    resize(frameDlibMmod, frameDlibMmodSmall, Size(inWidth, inHeight));
+
+    // Convert OpenCV image format to Dlib's image format
+    cv_image<bgr_pixel> dlibIm(frameDlibMmodSmall);
+    matrix<rgb_pixel> dlibMatrix;
+    assign_image(dlibMatrix, dlibIm);
+
+    // Detect faces in the image
+    std::vector<dlib::mmod_rect> faceRects = mmodFaceDetector(dlibMatrix);
+
+    for (size_t i = 0; i < faceRects.size(); i++)
+    {
+        int x1 = (int)(faceRects[i].rect.left() * scaleWidth);
+        int y1 = (int)(faceRects[i].rect.top() * scaleHeight);
+        int x2 = (int)(faceRects[i].rect.right() * scaleWidth);
+        int y2 = (int)(faceRects[i].rect.bottom() * scaleHeight);
+        cv::rectangle(frameDlibMmod, Point(x1, y1), Point(x2, y2), Scalar(0, 255, 0), (int)(frameHeight / 150.0), 4);
+    }
+}
+
+int main(int argc, const char** argv)
+{
+    faceCascadePath = "../models/haarcascade_frontalface_default.xml";
+
+    if (!faceCascade.load(faceCascadePath))
+    {
+        printf("--(!)Error loading face cascade\n");
         return -1;
     }
 
-    //VideoCapture cap(0);
-//  if (!cap.isOpened()) {
-  //      #      cerr << "ERROR: Could not open camera." << endl;
-    //    #      return -1;
-//#}
+    frontal_face_detector hogFaceDetector = get_frontal_face_detector();
+    String mmodModelPath = "../models/mmod_human_face_detector.dat";
+    net_type mmodFaceDetector;
+    deserialize(mmodModelPath) >> mmodFaceDetector;
 
-    // Load the cascades
-    CascadeClassifier faceCascade("../models/haarcascade_frontalface_alt.xml");
-    CascadeClassifier eyesCascade("../models/haarcascade_eye.xml"); // Try a different eye cascade
-
-    if (faceCascade.empty() || eyesCascade.empty()) {
-        cerr << "ERROR: Could not load cascades." << endl;
-        return -1;
+    string videoFileName;
+    string device;
+    string framework;
+    // Take arguments from command line
+    if (argc == 4)
+    {
+        videoFileName = argv[1];
+        device = argv[2];
+        framework = argv[3];
+    }
+    else if (argc == 3)
+    {
+        videoFileName = argv[1];
+        device = argv[2];
+        framework = "caffe";
+    }
+    else if (argc == 2)
+    {
+        videoFileName = argv[1];
+        device = "cpu";
+        framework = "caffe";
+    }
+    else
+    {
+        videoFileName = "";
+        device = "gpu";
+        framework = "caffe";
     }
 
-    // Avg width of a face in cm 
-    const float knownWidth = 14;
+    boost::to_upper(device);
+    cout << "OpenCV DNN Configuration:" << endl;
+    cout << "Device - " << device << endl;
+    if (framework == "caffe")
+        cout << "Framework - Caffe" << endl;
+    else
+        cout << "Framework - TensorFlow" << endl;
+    if (videoFileName == "")
+        cout << "No video found, using camera stream" << endl;
+    else
+        cout << "Video file - " << videoFileName << endl;
 
-    // Focal length of the camera in cm
-    const float focalLength = 600;
+    Net net;
 
-    Mat frame, gray;
-    vector<Rect> faces;
-    Rect lastFace;
-    int frameCounter = 0;
+    if (framework == "caffe")
+        net = cv::dnn::readNetFromCaffe(caffeConfigFile, caffeWeightFile);
+    else
+        net = cv::dnn::readNetFromTensorflow(tensorflowWeightFile, tensorflowConfigFile);
 
-    while (cap.read(frame)) {
-        cvtColor(frame, gray, COLOR_BGR2GRAY);
-        equalizeHist(gray, gray);
-
-        // Reduce frame size for faster processing
-        Mat smallGray;
-        float scale = 0.5;
-        resize(gray, smallGray, Size(), scale, scale);
-
-        // Detect faces every nth frame or if no face was detected in the last frame
-        if (frameCounter % 5 == 0 || lastFace.area() == 0) {
-            faceCascade.detectMultiScale(smallGray, faces, 1.1, 3, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
-            if (!faces.empty()) {
-                lastFace = faces[0];
-            }
-        }
-        else {
-            // Search in the vicinity of the last detected face
-            Rect searchRegion = lastFace + Size(lastFace.width / 2, lastFace.height / 2);
-            searchRegion -= Point(searchRegion.width / 4, searchRegion.height / 4);
-            searchRegion &= Rect(0, 0, smallGray.cols, smallGray.rows); // Ensure ROI is within image bounds
-
-            Mat roiGray = smallGray(searchRegion);
-            faceCascade.detectMultiScale(roiGray, faces, 1.1, 3, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
-
-            // Adjust face positions based on the search region
-            for (auto& face : faces) {
-                face += Point(searchRegion.x, searchRegion.y);
-                lastFace = face;
-            }
-        }
-
-        // Draw rectangles around the faces and detect eyes
-        for (const auto& face : faces) {
-            Rect scaledFace = face;
-            scaledFace.x /= scale;
-            scaledFace.y /= scale;
-            scaledFace.width /= scale;
-            scaledFace.height /= scale;
-
-            // Calculate and display the distance to the face
-            float distance = calculateDistance(scaledFace.width, knownWidth, focalLength);
-            putText(frame, to_string(std::round(distance)) + " cm", Point(scaledFace.x, scaledFace.y - 10), FONT_HERSHEY_SIMPLEX, 0.5, Scalar(255, 0, 0), 2);
-
-            // Draw a rectangle around the face
-            rectangle(frame, scaledFace, Scalar(255, 0, 0), 2);
-
-            // Limit the search area for eyes to the upper half of the face ROI
-            Mat faceROI = gray(scaledFace);
-            Rect eyeRegion(0, 0, scaledFace.width, scaledFace.height / 2);
-            Mat eyeROI = faceROI(eyeRegion);
-            vector<Rect> eyes;
-            eyesCascade.detectMultiScale(eyeROI, eyes, 1.1, 3, 0 | CASCADE_SCALE_IMAGE, Size(30, 30));
-
-            for (const auto& eye : eyes) {
-                // Draw a rectangle around each eye
-                Point eyeCenter(scaledFace.x + eye.x + eye.width / 2, scaledFace.y + eye.y + eye.height / 2);
-                rectangle(frame, Point(scaledFace.x + eye.x, scaledFace.y + eye.y), Point(scaledFace.x + eye.x + eye.width, scaledFace.y + eye.y + eye.height), Scalar(0, 255, 0), 2);
-            }
-        }
-
-        imshow("Face and Eye Tracking", frame);
-
-        if (waitKey(10) == 27) {
-            break; // Exit if ESC is pressed
-        }
-
-        frameCounter++;
+#if (CV_MAJOR_VERSION >= 4)
+    if (device == "CPU")
+    {
+        net.setPreferableBackend(DNN_TARGET_CPU);
+        cout << "Device - " << device << endl;
     }
+    else
+    {
+        net.setPreferableBackend(DNN_BACKEND_CUDA);
+        net.setPreferableTarget(DNN_TARGET_CUDA);
+        cout << "Device - " << device << endl;
+    }
+#elif(CV_MAJOR_VERSION == 3)
+    // OpenCV 3.4.x does not support GPU backend
+    net.setPreferableBackend(DNN_BACKEND_DEFAULT);
+    device = "cpu";
+    cout << "Device - " << device << endl;
+#endif
 
-    return 0;
+    cv::VideoCapture source;
+    if (videoFileName != "")
+        source.open(videoFileName);
+    else
+        source.open(0);
+
+    Mat frame;
+
+    int frame_count = 0;
+    double tt_opencvHaar = 0;
+    double tt_opencvDNN = 0;
+    double tt_dlibHog = 0;
+    double tt_dlibMmod = 0;
+
+    namedWindow("Face Detection Comparison", WINDOW_NORMAL);
+    double t = 0;
+    while (true)
+    {
+        source >> frame;
+        if (frame.empty())
+            break;
+
+        frame_count++;
+
+        t = cv::getTickCount();
+        Mat frameOpenCVHaar = frame.clone();
+        detectFaceOpenCVHaar(faceCascade, frameOpenCVHaar);
+        tt_opencvHaar += ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+        double fpsOpencvHaar = frame_count / tt_opencvHaar;
+        putText(frameOpenCVHaar, cv::format("OpenCV HAAR; FPS = %.2f", fpsOpencvHaar), Point(10, 50), FONT_HERSHEY_SIMPLEX, 1.3, Scalar(0, 0, 255), 4);
+
+        Mat frameOpenCVDNN = frame.clone();
+        t = cv::getTickCount();
+        detectFaceOpenCVDNN(net, frameOpenCVDNN, framework);
+        tt_opencvDNN += ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+        double fpsOpencvDNN = frame_count / tt_opencvDNN;
+        putText(frameOpenCVDNN, cv::format("OpenCV DNN %s FPS = %.2f", device.c_str(), fpsOpencvDNN), Point(10, 50), FONT_HERSHEY_SIMPLEX, 1.3, Scalar(0, 0, 255), 4);
+
+        t = cv::getTickCount();
+        Mat frameDlibHog = frame.clone();
+        detectFaceDlibHog(hogFaceDetector, frameDlibHog);
+        tt_dlibHog += ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+        double fpsDlibHog = frame_count / tt_dlibHog;
+        putText(frameDlibHog, cv::format("DLIB HoG; FPS = %.2f", fpsDlibHog), Point(10, 50), FONT_HERSHEY_SIMPLEX, 1.3, Scalar(0, 0, 255), 4);
+
+        t = cv::getTickCount();
+        Mat frameDlibMmod = frame.clone();
+        detectFaceDlibMMOD(mmodFaceDetector, frameDlibMmod);
+        tt_dlibMmod += ((double)cv::getTickCount() - t) / cv::getTickFrequency();
+        double fpsDlibMmod = frame_count / tt_dlibMmod;
+        putText(frameDlibMmod, cv::format("DLIB MMOD; FPS = %.2f", fpsDlibMmod), Point(10, 50), FONT_HERSHEY_SIMPLEX, 1.3, Scalar(0, 0, 255), 4);
+
+        Mat top, bottom, combined;
+        hconcat(frameOpenCVHaar, frameOpenCVDNN, top);
+        hconcat(frameDlibHog, frameDlibMmod, bottom);
+        vconcat(top, bottom, combined);
+
+        cv::resize(combined, combined, Size(), .5, .5);
+        imshow("Face Detection Comparison", combined);
+
+        int k = waitKey(5);
+        if (k == 27)
+        {
+            destroyAllWindows();
+            break;
+        }
+
+        if (frame_count == 1)
+        {
+            tt_opencvHaar = 0;
+            tt_opencvDNN = 0;
+            tt_dlibHog = 0;
+            tt_dlibMmod = 0;
+        }
+    }
 }
